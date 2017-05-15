2fbdab8b51b098dfaebc3e8d754ea75036e0db0a
adelowokanpelumijulius@gmail.com <Diamondchirt1995>
2017-05-15 11:49:02 +0100

    Added an external js file(Patch.js) Carousel TImeInterval changed to 4ms Carousel image made to full width Carousel Indicator removed Carousel Right and Left controllers removed Affix.js Imlemented

---
+++ js/patch.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+            $('button').bind('click', function( event ){
+               alert('Code: TUTERIA2017');
+            });
+         });
+
+             $(document).ready(function() {
+            $("em").click(function () {
+               $(this).replaceWith("<p><strong>Adelowokan Pelumi</strong></p>");
+            });
+         });
