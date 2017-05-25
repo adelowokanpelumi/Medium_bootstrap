ced82fc1df49299828423736dd0f09df054bd983
adelowokanpelumijulius@gmail.com <Diamondchirt1995>
2017-05-25 17:34:25 +0100

    Sass integration Medium Project  Re-design

---
+++ src/js/patch.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+            $('#coupon').bind('click', function( event ){
+               alert('Code: TUTERIA2017');
+            });
+         });
+
+             $(document).ready(function() {
+            $(".author").click(function () {
+            	event.preventDefault();
+               $(this).replaceWith("<p><strong>Adelowokan Pelumi</strong></p>");
+                
+            });
+         });
+
+          
